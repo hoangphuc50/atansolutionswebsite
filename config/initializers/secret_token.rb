a03ecdf59e26892212550ff7f59cc940750696a9
Phuc <phuc@hoangphuc-pc.(none)>
2013-10-30 15:10:30 +0700

    First upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AtanSolutionsWebsite::Application.config.secret_key_base = 'ccef5dd48749880c0ea80e609f84a2042d570881a2090d1d0f48b3c831a8d2e725594d887178f3c06ecbe87bc39b35f40c1b35bc54ea153b6c906b9aaca34d4a'

aad5000a901a24b608ec56493f589e9631125732
HoangTung <hoangtung@hoangtung-K43E.(none)>
2013-11-07 10:57:12 +0700

    Tung

--- app/models/article_language.rb
+++ app/models/article_language.rb
@@ -1,5 +1,5 @@
 class ArticleLanguage < ActiveRecord::Base
   belongs_to :article
   belongs_to :language
-  validates :title, presence: true   , length: { maximum: 50 }
+  validates :title, presence: true   , length: { maximum: 255 }
 end

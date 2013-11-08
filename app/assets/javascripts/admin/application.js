f72a4967511a236816da4b66a11accc286c755d4
HoangTung <hoangtung@hoangtung-K43E.(none)>
2013-11-08 16:34:43 +0700

    Tung editor production

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -1,8 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.min
-//= require home/jquery
-//= require kendo/kendo.web.min
 //= require admin/articles
 //= require admin/simpla.jquery.configuration
 
--- db/migrate/20131108020927_add_name_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToArticle < ActiveRecord::Migration
-  def change
-    add_column :articles, :name, :string
-  end
-end
